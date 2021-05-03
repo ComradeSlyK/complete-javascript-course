d4d90363787bd0281541a8059282d578736e96c3
ComradeSlyK <gregorini.silvio@gmail.com>
2021-05-03 22:33:45 +0200

    Video 13

--- 01-Fundamentals-Part-1/lesson/assignment.js
+++ 01-Fundamentals-Part-1/lesson/assignment.js
@@ -1,8 +1,8 @@
-let country = 'Italy';
-let continent = 'Europe';
-let population = '~70M';
-let isIsland = false;
-let language;
+const country = 'Italy';
+const continent = 'Europe';
+let population = 70000000;  // This might change in near future
+const isIsland = false;
+const language = 'Italian';
 
 console.log("Country: " + country + " " + typeof country);
 console.log("Continent: " + continent + " " + typeof continent);
