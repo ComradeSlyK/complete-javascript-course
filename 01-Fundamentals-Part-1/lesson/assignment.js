9d9b70d2f474039b1578d42d7a76d1af3e850bc1
ComradeSlyK <gregorini.silvio@gmail.com>
2021-05-03 21:56:46 +0200

    Video 10-11

---
+++ 01-Fundamentals-Part-1/lesson/assignment.js
@@ -0,0 +1,5 @@
+let country = 'Italy';
+let continent = 'Europe';
+let population = '~70M';
+
+console.log("Country " + country + ", Continent " + continent + ", Population " + population)
--- 01-Fundamentals-Part-1/lesson/script.js
+++ 01-Fundamentals-Part-1/lesson/script.js
@@ -2,3 +2,7 @@ let js = "Test";
 if (js === "Test")
     alert(js);
 console.log(40+8);
+
+let firstName = "A";
+console.log(firstName);
+ 
