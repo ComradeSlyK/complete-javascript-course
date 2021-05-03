30d6446a8fefa79cfbc2f5b38929aacbdbc66b21
ComradeSlyK <gregorini.silvio@gmail.com>
2021-05-03 23:26:24 +0200

    Video 17

--- 01-Fundamentals-Part-1/lesson/assignment.js
+++ 01-Fundamentals-Part-1/lesson/assignment.js
@@ -6,24 +6,24 @@ let population = 70 * (10 ** 6);  // This might change in near future
 const isIsland = false;
 const language = 'Italian';
 
-console.log("Country: " + country + " " + typeof country);
-console.log("Continent: " + continent + " " + typeof continent);
-console.log("Population: " + population + " " + typeof population);
-console.log("Is island: " + isIsland + " " + typeof isIsland);
-console.log("Language: " + language + " " + typeof language);
+console.log(`Country: ${country} (${typeof country})`);
+console.log(`Continent: ${continent} (${typeof continent})`);
+console.log(`Population: ${population} (${typeof population})`);
+console.log(`Is island: ${isIsland} (${typeof isIsland})`);
+console.log(`Language: ${language} (${typeof language})`);
 
 let halfPopulation = population / 2;
-console.log("Half population: " + population);
+console.log(`Half population: ${population}`);
 
 population++;
-console.log("Population: " + population);
+console.log(`Population: ${population}`);
 population--;
 
 const finlandPopulation = 6 * (10 ** 6);
-console.log(country + " has more people than Finland: " + population > finlandPopulation);
+console.log(`${country} has more people than Finland: ${population > finlandPopulation}`);
 
 const avgPopulation = 33 * (10 ** 6);
-console.log(country + " is more populated than the average: " + population > avgPopulation);
+console.log(`${country} is more populated than the average: ${population > avgPopulation}`);
 
-const description = country + " is in " + continent + ", and its " + population + " people speak " + language;
+const description = `${country} is in ${continent}, and its ${population} people speak ${language}`
 console.log(description)
