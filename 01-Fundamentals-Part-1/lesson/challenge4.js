74278ac37b9b1181624927c72a9ee61c46e505ae
SlyK182 <gregorini.silvio@gmail.com>
2021-05-20 23:49:12 +0200

    Video 29

---
+++ 01-Fundamentals-Part-1/lesson/challenge4.js
@@ -0,0 +1,15 @@
+console.log("challenge4.js")
+
+var bill;
+
+// Case 1
+bill = 275;
+bill >= 50 && bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${bill * 0.15}, and the total value ${bill * 1.15}`) : console.log(`The bill was ${bill}, the tip was ${bill * 0.2}, and the total value ${bill * 1.2}`)
+
+// Case 2
+bill = 40;
+bill >= 50 && bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${bill * 0.15}, and the total value ${bill * 1.15}`) : console.log(`The bill was ${bill}, the tip was ${bill * 0.2}, and the total value ${bill * 1.2}`)
+
+// Case 3
+bill = 430;
+bill >= 50 && bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${bill * 0.15}, and the total value ${bill * 1.15}`) : console.log(`The bill was ${bill}, the tip was ${bill * 0.2}, and the total value ${bill * 1.2}`)
